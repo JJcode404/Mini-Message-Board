7546f6663741bb02317599bbccb56c082a5899ad
JJcode404 <khamjapher1@gmail.com>
2025-03-07 22:41:28 +0300

    feat: populate database

---
+++ db/populateMessages.js
@@ -0,0 +1,34 @@
+import pkg from "pg";
+import "dotenv/config";
+const { Client } = pkg;
+
+const SQL = `
+CREATE TABLE messages (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  text TEXT NOT NULL,
+  user VARCHAR(255) NOT NULL,
+  profile VARCHAR(255),
+  added TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO messages (text, user, profile, added)  
+VALUES ("Good morning!", "Alice", "./profiles/profile1.jpg", NOW());
+
+`;
+
+async function main() {
+  console.log("seeding...");
+  const client = new Client({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    database: process.env.DATABASE_NAME,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.port,
+  });
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+  console.log("done");
+}
+
+main();
